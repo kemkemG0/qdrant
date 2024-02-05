951f5ae672dfbbc01d2f452872974a67284b5574
Roman Titov <ffuugoo@users.noreply.github.com>
2024-02-05 18:07:01 +0100

    Do not apply operation with `clock_tick` that is older than clock value in `ClockMap`

--- lib/collection/src/shards/local_shard/shard_ops.rs
+++ lib/collection/src/shards/local_shard/shard_ops.rs
@@ -133,10 +133,19 @@ impl ShardOperation for LocalShard {
             // TODO: We better lock `wal` before `clock_map`, but it does not work, because `wal` use `parking_lot::Mutex`... :/
 
             if let Some(clock_tag) = &mut operation.clock_tag {
-                self.clock_map
+                let prev_tick = self
+                    .clock_map
                     .lock()
                     .await
                     .advance_clock_and_correct_tag(clock_tag);
+
+                if clock_tag.clock_tick <= prev_tick && clock_tag.clock_tick > 0 {
+                    return Ok(UpdateResult {
+                        operation_id: None,
+                        status: UpdateStatus::Acknowledged,
+                        clock_tag: Some(clock_tag.clone()),
+                    });
+                }
             }
 
             let mut wal_lock = self.wal.lock();
